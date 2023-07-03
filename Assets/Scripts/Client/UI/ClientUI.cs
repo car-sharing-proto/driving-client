dd99f839b10705eda1e652951313233ae7b6ee37
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2023-07-04 01:30:17 +0300

    add pause icon

--- Assets/Scripts/Client/UI/ClientUI.cs
+++ Assets/Scripts/Client/UI/ClientUI.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 public class ClientUI : MonoBehaviour
 {
     [SerializeField] private GameObject _cursor;
+    [SerializeField] private GameObject _pauseBackground;
 
     private ClientIO _clientIO;
     private GameState _gameState;
 
-    public void Initialize(GameState _gameState, ClientIO clientIO)
+    public void Initialize(GameState gameState, ClientIO clientIO)
     {
-        this._gameState = _gameState;
+        this._gameState = gameState;
         this._clientIO = clientIO;
     }
 
     private void Update()
     {
-        _cursor.SetActive(_clientIO.IsFocused);
+        _cursor.SetActive(_clientIO.IsFocused && _gameState.IsUnpause);
+        _pauseBackground.SetActive(_gameState.IsPause);
 
         MouseController.SetVisibility(_gameState.IsPause);
     }
